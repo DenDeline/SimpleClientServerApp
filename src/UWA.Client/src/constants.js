559064f78295ca6767de35c184ce7418e7b98608
Rostislav <statko.rostik@gmail.com>
2020-05-16 15:34:32 +0300

    Initial commit

---
+++ src/UWA.Client/src/constants.js
@@ -0,0 +1,13 @@
+//endpoints and routes
+
+export const API_URL = 'localhost:5001/api';
+export const HOME_PAGE = '/';
+
+
+//methods
+export const GET = 'GET';
+export const POST = 'POST';
+export const PUT = 'PUT';
+export const DELETE = 'DELETE';
+
+export const GET_CURRENT_USER_INFO = '/users/currentUser';
